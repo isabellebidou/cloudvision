70702bf38011d0e505bc40544dd190e6f4a6d1e2
Isabelle <isa.bidou@gmail.com>
2021-04-29 12:26:34 +0200

    isabelle prining color properties

--- routes/visionapiroutes.js
+++ routes/visionapiroutes.js
@@ -2,25 +2,29 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/visioncall", function (req, res) {
-    async function quickstart() {
-        const vision = require('@google-cloud/vision');
-        // Imports the Google Cloud client library
-        // Creates a client
-const client = new vision.ImageAnnotatorClient();
+  async function quickstart() {
+    const vision = require("@google-cloud/vision");
+    // Imports the Google Cloud client library
+    // Creates a client
+    const client = new vision.ImageAnnotatorClient();
 
-/**
- * TODO(developer): Uncomment the following line before running the sample.
- */
- const fileName = '/Users/isabelle/cloudvision/resources/gallery.png';
+    /**
+     * TODO(developer): Uncomment the following line before running the sample.
+     */
+    const fileName = "/Users/isabelle/cloudvision/resources/gallery.png";
 
-// Performs property detection on the local file
-const [result] = await client.imageProperties(fileName);
-const colors = result.imagePropertiesAnnotation.dominantColors.colors;
-colors.forEach(color => console.log(color));
-      }
-      quickstart();
-    //res.render("visioncall");
-  });
+    // Performs property detection on the local file
+    const [result] = await client.imageProperties(fileName);
+    const colors = result.imagePropertiesAnnotation.dominantColors.colors;
+    //colors.forEach((color) => console.log(color));
+    console.log(colors[0].color.red)
+    console.log(colors[0].color.green)
+    console.log(colors[0].color.blue)
+    console.log(colors[0].score)
+    console.log(colors[0].pixelFraction)
+  }
+  quickstart();
+  //res.render("visioncall");
+});
 
-
-  module.exports = router;
+module.exports = router;
